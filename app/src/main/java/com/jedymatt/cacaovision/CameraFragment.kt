133a5f5d3dc5d2ef1970ea3c29e3e2f5f5b285ef
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-11-30 15:39:34 +0800

    Add tflite

---
+++ app/src/main/java/com/jedymatt/cacaovision/CameraFragment.kt
@@ -0,0 +1,199 @@
+package com.jedymatt.cacaovision
+
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.jedymatt.cacaovision.databinding.FragmentCameraBinding
+import org.tensorflow.lite.task.gms.vision.detector.Detection
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraFragment : Fragment(), ObjectDetectorHelper.DetectorListener {
+
+    private val TAG = "CameraFragment"
+
+    private var _fragmentCameraBinding: FragmentCameraBinding? = null
+
+    private val fragmentCameraBinding get() = _fragmentCameraBinding!!
+
+    private lateinit var objectDetectorHelper: ObjectDetectorHelper
+    private lateinit var bitmapBuffer: Bitmap
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    /** Blocking camera operations are performed using this executor */
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onResume() {
+        super.onResume()
+
+        // check permission
+    }
+
+    override fun onDestroyView() {
+        _fragmentCameraBinding = null
+        super.onDestroyView()
+
+        cameraExecutor.shutdown()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _fragmentCameraBinding = FragmentCameraBinding.inflate(inflater, container, false)
+
+        return fragmentCameraBinding.root
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        objectDetectorHelper = ObjectDetectorHelper(
+            context = requireContext(),
+            objectDetectorListener = this
+        )
+    }
+
+    private fun setUpCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(
+            {
+                // CameraProvider
+                cameraProvider = cameraProviderFuture.get()
+
+                // Build and bind the camera use cases
+                bindCameraUseCases()
+            },
+            ContextCompat.getMainExecutor(requireContext())
+        )
+    }
+
+    // Declare and bind preview, capture and analysis use cases
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun bindCameraUseCases() {
+
+        // CameraProvider
+        val cameraProvider =
+            cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
+
+        // CameraSelector - makes assumption that we're only using the back camera
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+        // Preview. Only using the 4:3 ratio because this is the closest to our models
+        preview =
+            Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+                .build()
+
+        // ImageAnalysis. Using RGBA 8888 to match how our models work
+        imageAnalyzer =
+            ImageAnalysis.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                // The analyzer can then be assigned to the instance
+                .also {
+                    it.setAnalyzer(cameraExecutor) { image ->
+                        if (!::bitmapBuffer.isInitialized) {
+                            // The image rotation and RGB image buffer are initialized only once
+                            // the analyzer has started running
+                            bitmapBuffer = Bitmap.createBitmap(
+                                image.width,
+                                image.height,
+                                Bitmap.Config.ARGB_8888
+                            )
+                        }
+
+                        detectObjects(image)
+                    }
+                }
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
+
+            // Attach the viewfinder's surface provider to preview use case
+            preview?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun detectObjects(image: ImageProxy) {
+        // Copy out RGB bits to the shared bitmap buffer
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+
+        val imageRotation = image.imageInfo.rotationDegrees
+        // Pass Bitmap and rotation to the object detector helper for processing and detection
+        objectDetectorHelper.detect(bitmapBuffer, imageRotation)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        imageAnalyzer?.targetRotation = fragmentCameraBinding.viewFinder.display.rotation
+    }
+
+    // Update UI after objects have been detected. Extracts original image height/width
+    // to scale and place bounding boxes properly through OverlayView
+    override fun onResults(
+        results: MutableList<Detection>?,
+        inferenceTime: Long,
+        imageHeight: Int,
+        imageWidth: Int
+    ) {
+        activity?.runOnUiThread {
+            // Pass necessary information to OverlayView for drawing on the canvas
+            fragmentCameraBinding.overlay.setResults(
+                results ?: LinkedList<Detection>(),
+                imageHeight,
+                imageWidth
+            )
+
+            // Force a redraw
+            fragmentCameraBinding.overlay.invalidate()
+        }
+    }
+
+    override fun onError(error: String) {
+        activity?.runOnUiThread {
+            Toast.makeText(requireContext(), error, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onInitialized() {
+        objectDetectorHelper.setupObjectDetector()
+        // Initialize our background executor
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        // Wait for the views to be properly laid out
+        fragmentCameraBinding.viewFinder.post {
+            // Set up the camera and its use cases
+            setUpCamera()
+        }
+
+        fragmentCameraBinding.progressCircular.visibility = View.GONE
+    }
+}
---
+++ app/src/main/java/com/jedymatt/cacaovision/ObjectDetectorHelper.kt
@@ -0,0 +1,131 @@
+package com.jedymatt.cacaovision
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.SystemClock
+import android.util.Log
+import com.google.android.gms.tflite.client.TfLiteInitializationOptions
+import com.google.android.gms.tflite.gpu.support.TfLiteGpu
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.Rot90Op
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.gms.vision.TfLiteVision
+import org.tensorflow.lite.task.gms.vision.detector.Detection
+import org.tensorflow.lite.task.gms.vision.detector.ObjectDetector
+
+class ObjectDetectorHelper(
+    var threshold: Float = 0.5f,
+    var numThreads: Int = 2,
+    var maxResults: Int = 3,
+    var currentDelegate: Int = 0,
+    val context: Context,
+    val objectDetectorListener: DetectorListener
+) {
+    private val TAG = "ObjectDetectionHelper"
+
+    private var objectDetector: ObjectDetector? = null
+    private var gpuSupported = false
+    private val modelPath = "mobilenetv1.tflite"
+
+    init {
+
+        TfLiteGpu.isGpuDelegateAvailable(context).onSuccessTask { gpuAvailable: Boolean ->
+            val optionsBuilder = TfLiteInitializationOptions.builder()
+            if (gpuAvailable) {
+                optionsBuilder.setEnableGpuDelegateSupport(true)
+            }
+            TfLiteVision.initialize(context, optionsBuilder.build())
+        }.addOnSuccessListener {
+            objectDetectorListener.onInitialized()
+        }.addOnFailureListener {
+            objectDetectorListener.onError("TfLiteVision failed to initialize: " + it.message)
+        }
+    }
+
+    fun clearObjectDetector() {
+        objectDetector = null
+    }
+
+    fun setupObjectDetector() {
+        if (!TfLiteVision.isInitialized()) {
+            Log.e(TAG, "setupObjectDetector: TfLiteVision is not initialized yet")
+        }
+
+        val optionsBuilder = ObjectDetector.ObjectDetectorOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+
+        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(numThreads)
+
+        when (currentDelegate) {
+            DELEGATE_CPU -> {
+                // default
+            }
+            DELEGATE_GPU -> {
+                if (gpuSupported) {
+                    baseOptionsBuilder.useGpu()
+                } else {
+                    objectDetectorListener.onError("GPU is not supported on this device")
+                }
+            }
+            DELEGATE_NNAPI -> {
+                baseOptionsBuilder.useNnapi()
+            }
+        }
+
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        try {
+            objectDetector = ObjectDetector.createFromFileAndOptions(
+                context,
+                modelPath,
+                optionsBuilder.build()
+            )
+        } catch (e: Exception) {
+            objectDetectorListener
+                .onError("Object detector failed to initialize. See error logs for details")
+            Log.e(TAG, "TFLite failed to load model with error: " + e.message)
+        }
+    }
+
+    fun detect(image: Bitmap, imageRotation: Int) {
+        if (!TfLiteVision.isInitialized()) {
+            Log.e(TAG, "detect: TfLiteVision is not initialized yet")
+            return
+        }
+
+        if (objectDetector == null) {
+            setupObjectDetector()
+        }
+
+        var inferenceTime = SystemClock.uptimeMillis()
+
+        val imageProcessor = ImageProcessor.Builder().add(Rot90Op(-imageRotation / 90)).build()
+
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(image))
+
+        val results = objectDetector?.detect(tensorImage)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+        objectDetectorListener.onResults(
+            results,
+            inferenceTime,
+            tensorImage.height,
+            tensorImage.width
+        )
+    }
+
+    interface DetectorListener {
+        fun onInitialized()
+        fun onError(error: String)
+        fun onResults(
+            results: MutableList<Detection>?, inferenceTime: Long, imageHeight: Int, imageWidth: Int
+        )
+    }
+
+    companion object {
+        const val DELEGATE_CPU = 0
+        const val DELEGATE_GPU = 1
+        const val DELEGATE_NNAPI = 2
+    }
+}
