40c21c4a29159776946a2e00f8d1770c15600486
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-11-30 15:39:58 +0800

    Render CameraFragment

--- app/src/main/java/com/jedymatt/cacaovision/CameraActivity.kt
+++ app/src/main/java/com/jedymatt/cacaovision/CameraActivity.kt
@@ -7,5 +7,13 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.camera_fragment_container, CameraFragment())
+                .setReorderingAllowed(true)
+                .commit()
+        }
     }
 }
