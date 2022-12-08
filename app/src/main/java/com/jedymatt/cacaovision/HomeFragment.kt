5822795d1804d0e52b8c7993a9d21b473c2ca707
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-12-08 20:11:10 +0800

    Detect object from images in gallery

--- app/src/main/java/com/jedymatt/cacaovision/HomeFragment.kt
+++ app/src/main/java/com/jedymatt/cacaovision/HomeFragment.kt
@@ -1,23 +1,71 @@
 package com.jedymatt.cacaovision
 
-import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.google.android.gms.tflite.client.TfLiteInitializationOptions
+import com.google.android.gms.tflite.gpu.support.TfLiteGpu
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.gms.vision.TfLiteVision
+import org.tensorflow.lite.task.gms.vision.detector.ObjectDetector
 
 
 class HomeFragment : Fragment() {
+    private val TAG = "HomeFragement"
+
     private val fileChooser =
         registerForActivityResult(ActivityResultContracts.GetContent()) { imgUri ->
             if (imgUri != null) {
-                // do something with the image
+                // get image from imgUri
+                var image: Bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    ImageDecoder.decodeBitmap(
+                        ImageDecoder.createSource(
+                            requireContext().contentResolver, imgUri
+                        )
+                    ).copy(Bitmap.Config.ARGB_8888, true)
+                } else {
+                    MediaStore.Images.Media.getBitmap(requireContext().contentResolver, imgUri)
+                }
+
+//                lifecycleScope.launch(Dispatchers.Default) {
+                runObjectDetection(image)
+//                }
             }
         }
+
+    private fun runObjectDetection(bitmap: Bitmap) {
+
+        TfLiteGpu.isGpuDelegateAvailable(context).onSuccessTask { gpuAvailable: Boolean ->
+            val optionsBuilder = TfLiteInitializationOptions.builder()
+            if (gpuAvailable) {
+                optionsBuilder.setEnableGpuDelegateSupport(true)
+            }
+            TfLiteVision.initialize(context, optionsBuilder.build())
+        }.addOnSuccessListener {
+            val image = TensorImage.fromBitmap(bitmap)
+
+            val options = ObjectDetector.ObjectDetectorOptions.builder().setMaxResults(5)
+                .setScoreThreshold(0.5f).build()
+            val detector = ObjectDetector.createFromFileAndOptions(
+                requireContext(), "mobilenetv1.tflite", options
+            )
+
+            val results = detector.detect(image)
+            Log.i(TAG, results.toString())
+        }.addOnFailureListener {
+            Log.e(TAG, "TfLiteVision failed to initialize: " + it.message)
+        }
+    }
 
 
     override fun onCreateView(
